fe369a538c79c27de9e1d127e8eb2045ddc55d97
Andrea Fercia <a.fercia@gmail.com>
2024-03-20 16:24:03 +0100

    Make sure social links are not empty.

--- packages/block-library/src/social-link/index.php
+++ packages/block-library/src/social-link/index.php
@@ -21,7 +21,7 @@ function render_block_core_social_link( $attributes, $content, $block ) {
 
 	$service     = ( isset( $attributes['service'] ) ) ? $attributes['service'] : 'Icon';
 	$url         = ( isset( $attributes['url'] ) ) ? $attributes['url'] : false;
-	$label       = ( isset( $attributes['label'] ) ) ? $attributes['label'] : block_core_social_link_get_name( $service );
+	$label       = ( ! empty( $attributes['label'] ) ) ? $attributes['label'] : block_core_social_link_get_name( $service );
 	$rel         = ( isset( $attributes['rel'] ) ) ? $attributes['rel'] : '';
 	$show_labels = array_key_exists( 'showLabels', $block->context ) ? $block->context['showLabels'] : false;
 
